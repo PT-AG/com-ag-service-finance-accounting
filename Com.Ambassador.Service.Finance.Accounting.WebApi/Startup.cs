4f8d45d8b8f1b83416fb36f2679cab448ff9c2bb
AndreaZain <andrea.kinan@gmail.com>
2023-06-21 09:32:11 +0700

    Create-Adjutment-UAT
    Create-Adjutment

--- Com.Ambassador.Service.Finance.Accounting.WebApi/Startup.cs
+++ Com.Ambassador.Service.Finance.Accounting.WebApi/Startup.cs
@@ -106,6 +106,7 @@ using Com.Ambassador.Service.Finance.Accounting.Lib.BusinessLogic.GarmentFinance
 using Com.Ambassador.Service.Finance.Accounting.Lib.BusinessLogic.GarmentFinance.Reports.LocalBankCashReceiptMonthlyRecap;
 using Com.Ambassador.Service.Finance.Accounting.Lib.BusinessLogic.Reports.GarmentExportBankReceiptJournal;
 using Com.Ambassador.Service.Finance.Accounting.Lib.BusinessLogic.Reports.GarmentLocalBankReceiptJournal;
+using Com.Ambassador.Service.Finance.Accounting.Lib.BusinessLogic.GarmentFinance.Adjustment;
 
 namespace Com.Ambassador.Service.Finance.Accounting.WebApi
 {
@@ -213,7 +214,8 @@ namespace Com.Ambassador.Service.Finance.Accounting.WebApi
                 .AddTransient<IGarmentFinanceExportSalesOutstandingReportService, GarmentFinanceExportSalesOutstandingReportService>()
                 .AddTransient<IGarmentFinanceLocalBankCashReceiptMonthlyRecapService, GarmentFinanceLocalBankCashReceiptMonthlyRecapService>()
                 .AddTransient<IGarmentExportBankReceiptJournalService, GarmentExportBankReceiptJournalService>()
-                .AddTransient<IGarmentLocalBankReceiptJournalService, GarmentLocalBankReceiptJournalService>();
+                .AddTransient<IGarmentFinanceAdjustmentService, GarmentFinanceAdjustmentService>()     
+               .AddTransient<IGarmentLocalBankReceiptJournalService, GarmentLocalBankReceiptJournalService>();
         }
 
 
@@ -324,11 +326,11 @@ namespace Com.Ambassador.Service.Finance.Accounting.WebApi
                 app.UseDeveloperExceptionPage();
             }
 
-            using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
-            {
-                var context = serviceScope.ServiceProvider.GetService<FinanceDbContext>();
-                context.Database.Migrate();
-            }
+            //using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            //{
+            //    var context = serviceScope.ServiceProvider.GetService<FinanceDbContext>();
+            //    context.Database.Migrate();
+            //}
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
